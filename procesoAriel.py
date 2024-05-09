968c414b25c857805fb1442b8f2e79e0fc2ca386
Ariel <arigomezulloa@gmail.com>
2024-05-09 16:19:48 -0500

    proyecto

--- procesoAriel.py
+++ procesoAriel.py
@@ -220,7 +220,3 @@ primerV3=Item.objects.filter(status=True)[0]
 primerV4=Item.objects.filter(status=True).order_by('-id')[0]
 
 total_devices=ItemUnidadMedidaStock.objects.filter(itemunidadmedida__item__marca_id=9, status=True).aggregate(total=Avg('stock'))['total']
-
-
-
-
